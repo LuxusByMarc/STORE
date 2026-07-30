5dd06be99d9ec07ad86782decc6cbfdf9db8a2d9
LuxusByMarc <neuron-curvy-09@icloud.com>
2026-07-30 11:18:27 -0700

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -24,10 +24,15 @@ FOOTER
 
         <p class="text-white mt-3" style="max-width:700px;margin:30px auto;opacity:.9;">
 
-  Stöbere in unseren Kategorien und suche dir deine Wunschartikel aus. Notiere dir die Artikelnummern oder sende uns einen Screenshot.
-  Kontaktiere uns ganz einfach über Whatsapp – Dort wirst du persönlich auf Deutsch betreut.
-  Nach deiner Auswahl erhältst du alle Details zur Bezahlung sowie eine Bestellbestätigung. Wir kümmern uns um den Rest.
-  Jetzt kannst du dich entspannt zurücklehnen – dein Paket kommt sicher bei dir an. 
+ Stöbere durch unsere Kategorien und entdecke unser Sortiment. Solltest du einen 
+ bestimmten Artikel nicht finden, kontaktiere uns einfach – viele Produkte sind 
+ noch nicht auf der Webseite gelistet. Sende uns einfach einen Screenshot bequem 
+ per WhatsApp. Dort wirst du persönlich auf Deutsch 
+ beraten und erhältst alle Informationen zu Verfügbarkeit, Lieferzeit sowie den 
+ verfügbaren Zahlungsmöglichkeiten. Nach deiner Bestellung kümmern wir uns um 
+ den Rest. Lagernde Artikel werden direkt aus Deutschland versendet und sind in 
+ der Regel innerhalb von 3 Werktagen bei dir. Bei nicht lagernden Artikeln teilen 
+ wir dir die voraussichtliche Lieferzeit selbstverständlich vorab mit.
 
         </p>
 
